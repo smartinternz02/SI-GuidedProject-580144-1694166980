bffe6ace91bb01f3032b121d43f2001bae18d810
Garima Singh <96335472+GarimaSingh2706@users.noreply.github.com>
2023-09-22 10:03:43 +0530

    Update ExampleInstrumentedTest.kt

--- smartInterzAssignment1/app/src/androidTest/java/garima/smartinterzassignment1/ExampleInstrumentedTest.kt
+++ smartInterzAssignment1/app/src/androidTest/java/garima/smartinterzassignment1/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package arush.smartinterzassignment1
+package garima.smartinterzassignment1
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
