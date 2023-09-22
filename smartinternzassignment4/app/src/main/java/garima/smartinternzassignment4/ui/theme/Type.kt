99e34ea5713f38fee67ae9144df76e17a1c4d2ff
Garima Singh <96335472+GarimaSingh2706@users.noreply.github.com>
2023-09-22 09:54:40 +0530

    Update Type.kt

--- smartinternzassignment4/app/src/main/java/garima/smartinternzassignment4/ui/theme/Type.kt
+++ smartinternzassignment4/app/src/main/java/garima/smartinternzassignment4/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package arush.smartinternzassignment4.ui.theme
+package garima.smartinternzassignment4.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
